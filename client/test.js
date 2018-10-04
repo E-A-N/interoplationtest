f67254d7fdcca580e1b3ffcf035f77a7ab0fdc7d
Cyborg_Ean <ean.director@gmail.com>
2018-10-03 18:11:19 -0700

    setting up client

---
+++ client/test.js
@@ -0,0 +1,11 @@
+
+window.onload = () => {
+    const msg = document.getElementById("msgWin");
+    const gameArea = document.getElementById("gameContainer");
+    const can = document.createElement("canvas");
+    const ctx = can.getContext("2d");
+
+
+
+
+}
