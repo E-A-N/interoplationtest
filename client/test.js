40924c6e8db7db864f32d8e49a766ea327c2ff20
Cyborg_Ean <ean.director@gmail.com>
2018-10-04 07:17:59 -0700

    implemented basic counter

--- client/test.js
+++ client/test.js
@@ -4,8 +4,19 @@ window.onload = () => {
     const gameArea = document.getElementById("gameContainer");
     const can = document.createElement("canvas");
     const ctx = can.getContext("2d");
+    let num = 0;
+    can.height = 500;
+    can.width  = 500;
+
+    const count1 = ctx.fillText("Hey", can.width/2, can.height/2);
 
 
 
+    gameArea.appendChild(can)
 
+    setInterval(function(){
+        ctx.clearRect(0, 0, can.width, can.height);
+        let tex = "Hey, num is: " + num++;
+        const count1 = ctx.fillText(tex, can.width * 0.25, can.height * 0.25);
+    },500)
 }
