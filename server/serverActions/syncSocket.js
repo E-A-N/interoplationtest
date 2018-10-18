8b22d0e9569c20f3d8c24f3dccd1442da71aa611
Cyborg_Ean <ean.director@gmail.com>
2018-10-17 18:36:33 -0700

    adjusted sync socket module to recieve the server as a parameter

--- server/serverActions/syncSocket.js
+++ server/serverActions/syncSocket.js
@@ -1,14 +1,17 @@
-/**
-* This method updates a chosen socket with the state of the other sockets
-* @param {object} soc - The chosen socket to updates
-* @param {object} socList - Associative array containing relevant sockets
-*/
-const sync =  (soc, socList) => {
-    for (let s in socList){
-        let update = socList[s];
-        soc.game.sockets[update.id] = update;
-    }
-    return soc;
-}
 
-module.exports = sync;
+module.exports = (server) => {
+    /**
+    * This method updates a chosen socket with the state of the other sockets
+    * @param {object} soc - The chosen socket to updates
+    * @param {object} socList - Associative array containing relevant sockets
+    */
+    server._syncSocket = function(soc, socList){
+        for (let s in socList){
+            let update = socList[s];
+            soc.game.sockets[update.id] = update;
+        }
+        return soc;
+    }
+
+    return server
+}
--- server/websockets.js
+++ server/websockets.js
@@ -11,7 +11,7 @@ module.exports = function(io) {
     const onConnection = require("./serverActions/onConnection")(io);
     const onDisconnect = require("./serverActions/onDisconnect")(io);
     const onJoinGame   = require("./serverActions/onJoinGame")(io);
-    const syncSocket   = require("./serverActions/syncSocket");
+    const syncSocket   = require("./serverActions/syncSocket")(io);
 
     //When a new user connects
     io.on("connection", onConnection);
@@ -23,15 +23,15 @@ module.exports = function(io) {
     io.on("joinGame", onJoinGame);
 
     const updateGameLoop = () => {
-        io._updateTimer(ups, io._sockets);
+        io._updateTimer(updateRate, io._sockets);
         //io.updateActionHistory(io.sockets);
         for (let s in io._sockets) {
             let soc = io._sockets[s];
-            syncSocket(soc, io._sockets);
+            io._syncSocket(soc, io._sockets);
         }
         //io.emit("gameUpdate", io._sockets);
     }
-    // io._createDummy(onConnection);
+     io._createDummy(onConnection);
     //eanDebug decide the best way to start and store gameloop in a variable
     /**
      * @param {number} - ups: Updates Per Second
