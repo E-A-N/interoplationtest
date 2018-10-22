5c3620c46ccb7894840a29267dff5cfb1bd45715
Cyborg_Ean <ean.director@gmail.com>
2018-10-22 12:42:02 -0700

    Update socketUpdate.js

--- server/serverActions/socketUpdate.js
+++ server/serverActions/socketUpdate.js
@@ -1,8 +1,6 @@
 module.exports = (socket) => {
 
     socket.on("socketUpdate", (data) => {
-        const deltaPacket = Object.keys(data);
-
         for (let i in data){
             //eanDebug check to make sure data attibutes exist in game
             socket.game[i] = data[i];
