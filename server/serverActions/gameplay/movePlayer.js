7d8363a182830b00529d4dfcb9da5b31c8dcad68
Cyborg_Ean <ean.director@gmail.com>
2018-10-23 06:57:18 -0700

    setting up inputs on server side

--- server/serverActions/gameplay/movePlayer.js
+++ server/serverActions/gameplay/movePlayer.js
@@ -1,3 +1,27 @@
 module.export = (socket) => {
+    const ox = socket.game.x;
+    const oy = socket.game.y;
+    var vspd = socket.game.vspd;
+    var hspd = socket.game.hspd;
 
+    //stop moving if there's no input;
+    socket.game.x = 0;
+    socket.game.y = 0;
+
+    //check for inputs
+    if (socket.game.leftInput){
+        socket.game.x += -hspd;
+    }
+
+    if (socket.game.rightInput){
+        socket.game.x += -hspd;
+    }
+
+    if (socket.game.upInput){
+        socket.game.y += -vspd;
+    }
+
+    if (socket.game.downInput){
+        socket.game.y += vspd;
+    }
 }
