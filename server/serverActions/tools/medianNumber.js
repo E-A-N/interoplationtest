252a31e32d867fdf7419fedb010b6c700c851cd5
Cyborg_Ean <ean.director@gmail.com>
2018-10-26 08:42:28 -0700

    testing median algorithm

--- server/serverActions/tools/medianNumber.js
+++ server/serverActions/tools/medianNumber.js
@@ -1,4 +1,5 @@
 module.export = (numbers) => {
+    let median;
     numbers = numbers.sort((a,b) => {
         return a - b;
     });
@@ -6,9 +7,13 @@ module.export = (numbers) => {
     const isEvenNumber = numbers.length % 2 === 0;
 
     if (isEvenNumber){
-
+        const central1 = numbers[(numbers.length/2) - 1]
+        const central2 = numbers[(numbers.length/2)];
+        median = (central1 + central2)/2;
     }
     else {
-        
+        median = (numbers.length +1)/2;
     }
+
+    return median;
 }
