bbbd54229dcbcbe8e3868863267cd79a61a99c4d
Cyborg_Ean <ean.director@gmail.com>
2018-10-25 22:12:59 -0700

    removing uncessary comments

--- server/serverActions/socketUpdate.js
+++ server/serverActions/socketUpdate.js
@@ -2,11 +2,8 @@ module.exports = (socket) => {
 
     socket.on("socketUpdate", (data) => {
         for (let i in data){
-            //eanDebug check to make sure data attibutes exist in game
             socket.game[i] = data[i];
         }
-
-        //console.log("recieved update:", data);
     });
 
     return socket
