2d1627a22bb689bcb8d7389e87da87b39a23426f
Cyborg_Ean <ean.director@gmail.com>
2018-10-26 08:01:17 -0700

    working on algorithm for median numbers

---
+++ server/serverActions/tools/medianNumber.js
@@ -0,0 +1,14 @@
+module.export = (numbers) => {
+    numbers = numbers.sort((a,b) => {
+        return a - b;
+    });
+
+    const isEvenNumber = numbers.length % 2 === 0;
+
+    if (isEvenNumber){
+
+    }
+    else {
+        
+    }
+}
