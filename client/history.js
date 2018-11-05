0be4925838e8ef5e74a5fd65fda98f0da56fb5be
Cyborg_Ean <ean.director@gmail.com>
2018-11-05 07:03:20 -0800

    working on client history module

---
+++ client/history.js
@@ -0,0 +1,16 @@
+const clientHistory = (cli) => {
+    cli.history = [];
+    cli.historySchema = {
+        time: 11332423230,
+        state: 2,
+        delta: {
+            time: 0,
+            position: 0
+        },
+        inputs: {},
+        position: {}
+    };
+    cli.
+
+
+}
